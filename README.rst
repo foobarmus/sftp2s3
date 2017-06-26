a331f0c9ec38821f796928aa914c8d8c8acb6074
Jonas McCallum <jonasmccallum@gmail.com>
2017-06-27 09:35:45 +1000

    refactor to increase clarity

---
+++ README.rst
@@ -0,0 +1,34 @@
+=======
+sftp2s3
+=======
+Copy a file from an SFTP server to an S3 bucket
+-------
+
+::
+
+    python sftp2s3.py mysftpserver.com:/path/to/file.png my-s3-bucketname
+    python3 sftp2s3.py mysftpserver.com:/path/to/file2.png my-s3-bucketname
+
+Installation
+-------
+
+::
+
+    git clone https://github.com/phatpiglet/sftp2s3.git
+
+Dependencies:
+
+- boto3 `config help`_
+- pysftp troubleshooting_
+
+Both dependencies can be installed with pip. However, installation can be tricky, especially if your system is a bit old.
+
+Boto3 also needs to be configured with:
+
+- credentials for a programmatic IAM user
+  with access to S3 and SES
+- a default region, that matches the
+  region of your S3 bucket
+
+.. _config help: http://boto3.readthedocs.io/en/latest/guide/quickstart.html
+.. _troubleshooting: https://stackoverflow.com/questions/22073516/failed-to-install-python-cryptography-package-with-pip-and-setup-py
--- sftp2s3.py
+++ sftp2s3.py
@@ -1,53 +1,63 @@
-# sftp2s3
-# copy files from an sftp server to an amazon s3 bucket
+# sftp2s3 - copy files from an sftp server to an S3 bucket
 # author: Jonas McCallum
 
 import os, argparse, getpass
 
 import pysftp, boto3
 
-def get_args():
-    """parse the command line arguments"""
-    parser = argparse.ArgumentParser()
-    parser.add_argument("source", help="host:/path/filename.etc")
-    parser.add_argument("dest", help="your-s3-bucketname")
-    args = parser.parse_args()
-    host, file_path = args.source.split(':')
-    filename = os.path.split(file_path)[-1]
-    bucket = args.dest
-    return args.source, host, file_path, filename, bucket
 
-def get_sftp_creds():
+class FileTransfer(object):
+    """file transfer controller"""
+
+    def __init__(self, args):
+        """parse the command line arguments"""
+        host, file_path = args.source.split(':')
+        filename = os.path.split(file_path)[-1]
+        self.host = host
+        self.file_path = file_path
+        self.filename = filename
+        self.bucket = args.dest
+        self.args = args
+
+    def sftp_get(self):
+        """download source file to local"""
+        credentials = _get_sftp_creds()
+        with pysftp.Connection(self.host, **credentials) as sftp:
+            sftp.get(self.file_path)
+
+    def s3_put(self):
+        """copy from local to s3 bucket"""
+        filename = self.filename
+        s3 = boto3.resource('s3')
+        with open(filename, 'rb') as f:
+            s3.Bucket(self.bucket).put_object(Key=filename, Body=f)
+
+    def cleanup(self):
+        """delete local copy of file"""
+        os.remove(self.filename)
+
+    def notify(self):
+        """send email notification of upload"""
+
+
+def _get_sftp_creds():
     """prompt for username and password"""
     return {'username': input('SFTP User: '),
             'password': getpass.getpass()}
 
-def sftp_get(host, file_path):
-    """download source file to local"""
-    credentials = get_sftp_creds()
-    with pysftp.Connection(host, **credentials) as sftp:
-        sftp.get(file_path)
-
-def s3_put(filename, bucket):
-    """copy from local to s3 bucket"""
-    s3 = boto3.resource('s3')
-    with open(filename, 'rb') as f:
-        s3.Bucket(bucket).put_object(Key=filename, Body=f)
-
-def cleanup(filename):
-    """delete local copy of file"""
-    os.remove(filename)
-
-def notify(source, dest):
-    """send email notification of upload"""
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("source", help="host:/path/filename.etc")
+    parser.add_argument("dest", help="your-s3-bucketname")
+    args = parser.parse_args()
     try:
-        source, host, file_path, filename, bucket = get_args()
-        sftp_get(host, file_path)
-        s3_put(filename, bucket)
-        notify(source, bucket)
-    except Exception as e:
-        print(e)
+        transfer = FileTransfer(args)
+        transfer.sftp_get()
+        transfer.s3_put()
+        transfer.notify()
     finally:
-        cleanup(filename)
+        try:
+            transfer.cleanup()
+        except NameError:
+            print('Nothing to clean up.')
